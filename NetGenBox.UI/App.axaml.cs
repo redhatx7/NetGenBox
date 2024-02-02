7838bc6a3014fa3dbd51a12494ec5eb34eab13e4
Amirreza Azadpour <amirreza_az@outlook.com>
2024-02-03 02:42:21 +0330

    Added DI

--- NetGenBox.UI/App.axaml.cs
+++ NetGenBox.UI/App.axaml.cs
@@ -2,16 +2,33 @@ using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
 using Microsoft.CodeAnalysis;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 
 namespace NetGenBox.UI;
 
 public partial class App : Application
 {
+    
+    private IServiceProvider _serviceProvider;
+    
     public override void Initialize()
     {
+        var serviceCollection = new ServiceCollection();
+        ConfigureServices(serviceCollection);
+        _serviceProvider = serviceCollection.BuildServiceProvider();
+        ServiceLocator.SetProvider(_serviceProvider);
+        ServiceLocator.SetConfiguration(Configuration);
         AvaloniaXamlLoader.Load(this);
+    }
+
+    private void ConfigureServices(ServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSingleton<MainWindow>();
+        serviceCollection.AddSingleton<ProjectConfiguration>();
     }
 
     public static IConfiguration Configuration =>
@@ -21,17 +38,13 @@ public partial class App : Application
     
     public override void OnFrameworkInitializationCompleted()
     {
-        Console.WriteLine("ISE PATH=" + Configuration["isePath"]);
+        //SukiTheme.GetInstance().ChangeBaseTheme(ThemeVariant.Light);
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            
-            var mainWindow = new MainWindow()
-            {
-                Configuration = Configuration,
-            };
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow.Configuration = Configuration;
             desktop.MainWindow = mainWindow;
         }
-
         base.OnFrameworkInitializationCompleted();
     }
 }
